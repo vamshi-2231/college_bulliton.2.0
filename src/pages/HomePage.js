887112be0d1769d2dfc035b204a946f5d592596e
Mamindla Vamshi <vamshimamindla1779@gmail.com>
2024-09-06 22:15:02 +0530

    first commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export default function HomePage() {
+  // Function to handle smooth scroll to the 'Projects' section
+  const scrollToUpdates = (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+    const targetElement = document.getElementById('projects');
+    
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <section id="home">
-    <div className="home container">
-      <div>
-        <h1>VAAGDEVI <span></span></h1>
-        <h1>COLLEGE OF<span></span></h1>
-        <h1>ENGINEERING<span></span></h1>
-        <a href="#projects" type="button" className="cta">Click for updates</a>
+      <div className="home container">
+        <div>
+          <h1>VAAGDEVI <span></span></h1>
+          <h1>COLLEGE OF<span></span></h1>
+          <h1>ENGINEERING<span></span></h1>
+          <a href="#projects" type="button" className="cta" onClick={scrollToUpdates}>
+            Click for updates
+          </a>
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
